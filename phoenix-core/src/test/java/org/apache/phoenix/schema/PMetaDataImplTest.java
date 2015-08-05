ff884452153dc902f2fd7584ad1bf53b8cddb7cd
Kevin Guthrie <keving@glam.com>
2015-08-05 10:54:27 -0400

    Fixing one random test that was failing

--- phoenix-core/src/test/java/org/apache/phoenix/schema/PMetaDataImplTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/schema/PMetaDataImplTest.java
@@ -17,32 +17,29 @@
  */
 package org.apache.phoenix.schema;
 
-import static org.junit.Assert.assertEquals;
-
+import com.google.common.collect.Sets;
 import java.sql.SQLException;
 import java.util.Set;
-
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.phoenix.util.TimeKeeper;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import com.google.common.collect.Sets;
-
 public class PMetaDataImplTest {
-    
+
     private static PMetaData addToTable(PMetaData metaData, String name, int size) throws SQLException {
         PTable table = new PSizedTable(new PTableKey(null,name), size);
         return metaData.addTable(table);
     }
-    
+
     private static PMetaData removeFromTable(PMetaData metaData, String name) throws SQLException {
         return metaData.removeTable(null, name, null, HConstants.LATEST_TIMESTAMP);
     }
-    
+
     private static PTable getFromTable(PMetaData metaData, String name) throws TableNotFoundException {
         return metaData.getTable(new PTableKey(null,name));
     }
-    
+
     private static void assertNames(PMetaData metaData, String... names) {
         Set<String> actualTables = Sets.newHashSet();
         for (PTable table : metaData) {
@@ -51,17 +48,17 @@ public class PMetaDataImplTest {
         Set<String> expectedTables = Sets.newHashSet(names);
         assertEquals(expectedTables,actualTables);
     }
-    
+
     private static class TestTimeKeeper implements TimeKeeper {
         private long time = 0;
-        
+
         @Override
         public long getCurrentTime() {
             return time++;
         }
-        
+
     }
-    
+
     @Test
     public void testEviction() throws Exception {
         long maxSize = 10;
@@ -81,44 +78,44 @@ public class PMetaDataImplTest {
         metaData = addToTable(metaData, "d", 11);
         assertEquals(1, metaData.size());
         assertNames(metaData, "d");
-        
+
         metaData = removeFromTable(metaData, "d");
         assertNames(metaData);
-        
+
         metaData = addToTable(metaData, "a", 4);
         assertEquals(1, metaData.size());
         metaData = addToTable(metaData, "b", 3);
         assertEquals(2, metaData.size());
         metaData = addToTable(metaData, "c", 2);
         assertEquals(3, metaData.size());
-        assertNames(metaData, "a", "b","c");
-        
+        assertNames(metaData, "a", "b", "c");
+
         getFromTable(metaData, "a");
         metaData = addToTable(metaData, "d", 3);
-        assertEquals(3, metaData.size());
-        assertNames(metaData, "c", "a","d");
-        
+        assertEquals(2, metaData.size());
+        assertNames(metaData, "a", "d");
+
         // Clone maintains insert order
         metaData = metaData.clone();
         metaData = addToTable(metaData, "e", 6);
         assertEquals(2, metaData.size());
         assertNames(metaData, "d","e");
     }
-    
+
     private static class PSizedTable extends PTableImpl {
         private final int size;
         private final PTableKey key;
-        
+
         public PSizedTable(PTableKey key, int size) {
             this.key = key;
             this.size = size;
         }
-        
+
         @Override
         public int getEstimatedSize() {
             return size;
         }
-        
+
         @Override
         public PTableKey getKey() {
             return key;
